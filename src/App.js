50768322bba36ca35f04c6c095cc861cba88aeac
Nelson <nelson.marcano@tunubi.com>
2020-04-19 21:12:38 -0300

    printing movie list

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React from 'react';
-import Movie from '../src/components/movies'
+import MovieList from '../src/components/movies'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          This is a movie.
-        </p>
-        <Movie />
+        <MovieList />
       </header>
     </div>
   );
--- src/components/movies/index.js
+++ src/components/movies/index.js
@@ -1,31 +1,41 @@
 import React, { Component } from 'react';
+import Movie from '../movie';
 
-class Movie extends Component {
+class MovieList extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      movie: { name: 'Test Movie' },
+      movies: [],
     };
   }
 
   componentDidMount() {
-    const api_url = process.env.REACT_APP_API_URL
+    const api_url = process.env.REACT_APP_API_URL + '/v1/movies'
     fetch(api_url)
       .then (response => response.json())
-      .then(movie => {
-        this.setState({ movie })
+      .then(movies => {
+        this.setState({ movies })
       }).catch(err => {
         console.log(err)
       })
   }
 
   render() {
+    let movies = []
+    for(let i = 0; i < this.state.movies.length; i++) {
+      movies.push(
+        <Movie key={i} movie={this.state.movies[i]} />
+      );
+    }
     return (
       <div className="shopping-list">
-        <h1>{this.state.movie.name}</h1>
+        <h1>List of movies</h1>
+        <ul>
+          {movies}
+        </ul>
       </div>
     );
   }
 }
 
-export default Movie;
+export default MovieList;
